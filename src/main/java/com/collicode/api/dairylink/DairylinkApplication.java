4d48512347d900b718bae392487375d33f2c77e6
c-kiplimo <kiplimocollins855@gmail.com>
2023-10-17 22:56:24 +0300

    project start

---
+++ src/main/java/com/collicode/api/dairylink/DairylinkApplication.java
@@ -0,0 +1,13 @@
+package com.collicode.api.dairylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairylinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairylinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collicode/api/dairylink/DairylinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collicode.api.dairylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairylinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
